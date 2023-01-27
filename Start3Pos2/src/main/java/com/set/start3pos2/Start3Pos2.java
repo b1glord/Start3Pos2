2780542f5407a0e581276756e02747897557aada
b1glord <FuryTr@yandex.com>
2023-01-27 13:39:08 +0300

    1

---
+++ Start3Pos2/src/main/java/com/set/start3pos2/Start3Pos2.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.set.start3pos2;
+
+/**
+ *
+ * @author burak
+ */
+public class Start3Pos2 {
+
+    public static void main(String[] args) {
+        //Start Login Form
+        Login login = new Login();
+        login.setVisible(true);
+    }        
+}
